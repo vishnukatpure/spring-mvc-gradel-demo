5d42f92db234de0531894780aebf7b13c240ad0e
Vishnu Katpure <vishnu.katpure@AD.MSYSTECHNOLOGIES.COM>
2019-11-07 11:02:32 +0530

    first commit

---
+++ src/main/java/com/dev/spring/controller/login/LoginController.java
@@ -0,0 +1,59 @@
+package com.dev.spring.controller.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class LoginController {
+
+	@RequestMapping(value = { "/" }, method = RequestMethod.GET)
+	public ModelAndView welcomePage() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("index");
+		return model;
+	}
+
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public ModelAndView homePage() {
+		ModelAndView model = new ModelAndView();
+		model.setViewName("home");
+		return model;
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView loginPage(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, HttpServletRequest request) {
+		request.getSession().invalidate();
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid Credentials provided.");
+		}
+
+		if (logout != null) {
+			model.addObject("message", "Logged out successfully.");
+		}
+
+		model.setViewName("index");
+		return model;
+	}
+
+	@RequestMapping(value = "/logout", method = RequestMethod.GET)
+	public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null) {
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		return "redirect:/login?logout=true";
+	}
+
+}
